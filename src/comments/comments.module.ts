843b36875344c7c3557339c6faf294a5acfef4d2
Maiten Blanc <maitengblanc@gmail.com>
2026-04-01 16:48:16 -0300

    add admin comment blocking

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -4,10 +4,11 @@ import { CommentsController } from './comments.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from 'src/auth/auth.module';
 import { Comment } from './entities/comment.entity';
+import { LockedEpisodes } from './entities/locked-episodes.entity';
 
 @Module({
   controllers: [CommentsController],
   providers: [CommentsService],
-  imports: [TypeOrmModule.forFeature([Comment]), AuthModule],
+  imports: [TypeOrmModule.forFeature([Comment, LockedEpisodes]), AuthModule],
 })
 export class CommentsModule {}
