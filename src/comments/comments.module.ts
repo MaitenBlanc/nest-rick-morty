3490b00d495e458f88dffbf51dce9ba2daf8ba18
Maiten Blanc <maitengblanc@gmail.com>
2026-04-01 14:49:43 -0300

    Update TypeOrmModule in comment.module.ts

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -3,11 +3,11 @@ import { CommentsService } from './comments.service';
 import { CommentsController } from './comments.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from 'src/auth/auth.module';
-import { Favorite } from 'src/favorites/entities/favorite.entity';
+import { Comment } from './entities/comment.entity';
 
 @Module({
   controllers: [CommentsController],
   providers: [CommentsService],
-  imports: [TypeOrmModule.forFeature([Favorite]), AuthModule],
+  imports: [TypeOrmModule.forFeature([Comment]), AuthModule],
 })
 export class CommentsModule {}
